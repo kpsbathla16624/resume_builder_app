625e4c501bdf01f9b26a19dcf8d763f4da781cf0
kamalpreet <kamalpreet6198@gmail.com>
2024-12-28 10:22:36 +0530

    trial

--- app/components/Step6.tsx
+++ app/components/Step6.tsx
@@ -38,6 +38,7 @@ function Step6() {
     const key = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
     try {
       const response = await fetch(
+        
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${key}`, 
         {
           method: "POST",
@@ -135,3 +136,4 @@ function Step6() {
 }
 
 export default Step6;
+
